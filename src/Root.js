b47658c018dd909ec748dd20ff5196e601a2f5c9
cieloazul310 <cieloazul310@gmail.com>
2018-01-26 17:59:06 +0900

    update

--- src/Root.js
+++ src/Root.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import configureStore from './configureStore';
+import { PersistGate } from 'redux-persist/lib/integration/react'
+
 import App from './App';
 import customMuiTheme from './customMuiTheme';
 
-const store = configureStore();
+let { store, persistor } = configureStore();
 
 export default class Root extends Component {
   render() {
     return (
       <MuiThemeProvider muiTheme={customMuiTheme}>
         <Provider store={store}>
-          <App />
+          <PersistGate loading={null} persistor={persistor}>
+            <App />
+          </PersistGate>
         </Provider>
       </MuiThemeProvider>
     )
--- src/configureStore.js
+++ src/configureStore.js
@@ -2,6 +2,8 @@ import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import { routerMiddleware } from 'react-router-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import createHistory from 'history/createBrowserHistory';
 import rootReducer from './reducers';
 
@@ -10,14 +12,27 @@ export const history = createHistory({
   basename: '/wikilayers/'
 });
 
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export default function configureStore(preloadedState) {
-  return createStore(
-    rootReducer,
+  let store =  createStore(
+    persistedReducer,
     preloadedState,
     applyMiddleware(
       thunkMiddleware,
       loggerMiddleware,
       routerMiddleware(history)
     )
-  )
+  );
+  let persistor = persistStore(store);
+
+  return {
+    store,
+    persistor
+  };
 };
