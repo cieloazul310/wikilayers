2b45772f346d6240840226c0bbeba7bae3b9f6f0
cieloazul310 <cieloazul310@gmail.com>
2021-02-04 21:13:20 +0900

    #26: update

--- src/layers/vtStyle/road.ts
+++ src/layers/vtStyle/road.ts
@@ -3,6 +3,9 @@ import Style from 'ol/style/Style';
 import Stroke from 'ol/style/Stroke';
 import { PaletteType } from '@material-ui/core';
 
+const res15 = 4.78;
+const res17 = 1.19;
+
 // motorway: 0 高速道路以外, 1 高速道路, 9 不明
 // rdCtg: 0 国道, 1 都道府県道, 2 市区町村道, 3 高速自動車国道等, 5 その他, 6 不明
 function roadColor(feature: FeatureLike, resolution: number, paletteType: PaletteType = 'light') {
@@ -18,7 +21,7 @@ function roadColor(feature: FeatureLike, resolution: number, paletteType: Palett
 
 // rnkWidth: 0: 3m未満, 1: 3m以上5.5m未満, 2: 5.5m以上13m未満
 // 3: 13m以上19.5m未満, 4: 19.5m以上 5: その他, 6: 不明
-function roadWidth(feature: FeatureLike, resolution: number) {
+function defaultRoadWidth(feature: FeatureLike) {
   const { rnkWidth, rdCtg } = feature.getProperties();
   if (rnkWidth === undefined) return rdCtg === 0 || rdCtg === 2 ? 2 : 1;
   if (rnkWidth === 4) return 3;
@@ -26,10 +29,17 @@ function roadWidth(feature: FeatureLike, resolution: number) {
   return rnkWidth === 1 ? 1 : 0.5;
 }
 
+function roadWidth(feature: FeatureLike, resolution: number) {
+  const width = defaultRoadWidth(feature);
+  return resolution > res17 ? width * Math.max(res15 / resolution, 1) : 2;
+}
+
 export default function roadStyle(feature: FeatureLike, resolution: number, paletteType: PaletteType = 'light'): Style {
   const color = roadColor(feature, resolution, paletteType);
   const width = roadWidth(feature, resolution);
-  const { lvOrder } = feature.getProperties();
+  const { lvOrder, rdCtg } = feature.getProperties();
+
+  if (resolution < res17 && rdCtg !== undefined) return new Style();
   return new Style({
     stroke: new Stroke({
       width,
